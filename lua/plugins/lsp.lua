e5f82c592f48ac2fcdf23fc91e15c4d45e5ea073
wuzijun <wuzijun@bigai.ai>
2023-07-24 22:30:57 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,8 +4,8 @@ return -- LSP keymaps
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- change a keymap
-    keys[#keys + 1] = { "gr", "<cmd>Trouble lsp_references<cr>", "Go to references(Trouble)" }
-    keys[#keys + 1] = { "gd", "<cmd>Trouble lsp_definitions<cr>", "Go to definitions(Trouble)" }
+    keys[#keys + 1] = { "gr", false }
+    keys[#keys + 1] = { "gd", false }
     -- disable a keymap
     -- keys[#keys + 1] = { "K", false }
     -- add a keymap
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "gr", "<cmd>Trouble lsp_references<cr>", "Go to references(Trouble)" },
+    { "gd", "<cmd>Trouble lsp_definitions<cr>", "Go to definitions(Trouble)" },
+  },
+}
