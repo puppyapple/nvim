70f35fbc469e3918ebe245c73a6c8728afa72d9b
wuzijun <wuzijun@bigai.ai>
2023-07-26 14:24:00 +0800

    update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,1 +1,4 @@
-return { "nvim-neo-tree/neo-tree.nvim", enabled = false }
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+}
