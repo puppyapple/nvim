652ce65fa82ff66b1a5325d84a233cda460c90a1
wuzijun <wuzijun@bigai.ai>
2023-09-21 00:25:06 +0800

    add copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,23 +1,39 @@
 return {
   "zbirenbaum/copilot.lua",
-  opts = {
-    panel = {
-      enabled = false,
-      auto_refresh = true,
-      keymap = {
-        jump_prev = "[c",
-        jump_next = "]c",
-        accept = "<C-i>",
-        refresh = "gc",
-        open = "<C-c>"
-      }
-    },
-    filetypes = {
-      python = true
-    },
-    suggestions = {
-      enabled = false,
-      auto_trigger = true,
-    },
-  },
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        accept = false, -- disable built-in keymapping
+        keymap = {
+          jump_prev = "[c",
+          jump_next = "]c",
+          accept = "<C-i>",
+          refresh = "gc",
+          open = "<C-c>"
+        }
+      },
+    })
+
+    -- hide copilot suggestions when cmp menu is open
+    -- to prevent odd behavior/garbled up suggestions
+    -- local cmp_status_ok, cmp = pcall(require, "cmp")
+    -- if cmp_status_ok then
+    --   cmp.event:on("menu_opened", function()
+    --     vim.b.copilot_suggestion_hidden = true
+    --   end)
+    --
+    --   cmp.event:on("menu_closed", function()
+    --     vim.b.copilot_suggestion_hidden = false
+    --   end)
+    -- end
+  end,
 }
